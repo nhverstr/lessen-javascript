e7ede8afbf32020f2332f82d508be46d4455670e
Nathan Verstraelen <nathanverstraelen@gmail.com>
2024-04-20 10:37:07 +0200

    les 4

---
+++ les-4/script.js
@@ -0,0 +1,78 @@
+/**
+ * Beste mol, in les 4 werk je niet in de frontend, maar in de backend.
+ *
+ * dwz: Je javascript runt niet in een index.html, en je klikt niet op 'go live' om die index.html
+ * te hosten in je browser.
+ *
+ * Nee, je javascript runt nu in je terminal (op de server als het ware) en je gebruikt
+ * #> node script.js
+ * om het uit te voeren.
+ *
+ * Output gebeurt steeds met console.log
+ *
+ */
+
+// JS Array refence
+/**
+ * Deze les steunt op een heel belangrijke pagina, die javascript array reference (of arr ref in het kort). Dit is een lijst
+ * die elke functie de er bestaat, geordend op lijst. Je moet die niet uit je hoofd kennen, maar deze wel
+ * extensief gebruiken. (https://www.w3schools.com/jsref/jsref_obj_array.asp) (en dus uit je hoofd kennen)
+ * (deze pagina is mijn lievelingspagina)
+ */
+
+const natureArr = ["tree", "river", "mountain", "flower"];
+const techArr = [
+  "computer",
+  "internet",
+  "software",
+  "algorithm",
+  "data",
+  "network",
+  "code",
+  "device",
+];
+
+console.log("Nature Words:", natureWords);
+console.log("Tech Words:", techWords);
+
+/**
+ * Deel 1: vind een functie in de array ref die de array verandert naar een string
+ * Print die string
+ */
+
+// code deel 1
+
+/**
+ * Deel 2: filter() vs reduce()
+ * Neem alle woorden van natureArr, en maak een nieuw lijst 'shortNatureArr'
+ * In shortNature komen alle woorden met minder dan 6 letters
+ * Print shortNature, met de methode die je leerde in deel 1
+ */
+
+// code deel 2
+
+/**
+ * Deel 3
+ * Output het laatste woord met 4 letters in techArr
+ * Output techArr zonder dat laatste woord
+ * 
+ * Output de reverse van deze array.
+ * 
+ * Output het eerste woord met 4 letters in techArr
+ * Output techArr zonder dat eerste woord
+ * 
+ * Tip: als je deze allemaal na elkaar uitvoert, kom je in de problemen. Gebruik een temporary array
+ * om je originele bewaren
+ */
+
+// code deel 3
+
+/**
+ * Deel 4
+ * Wat is het verschil tussen .includes() en .indexOf()?
+ * Test dit, door te zoeken naar het woord 'flower' in natureArr.
+ * Output het resultaat van .includes() op 'flower'
+ * Output het resultaat van .indexOf() op 'flower'
+ */
+
+// code deel 4
---
+++ notes/notes.md
@@ -0,0 +1,54 @@
+# Notities van de leraar
+
+## ideetjes
+
+- backend API?
+- extra oef array bewerkingen frontend?
+
+## JS Array
+
+filter() vs reduce()
+
+forEach() vs map()
+
+includes() vs indexOf()
+
+join()
+
+lastIndexOf() ? eventueel
+
+pop() vs shift()
+
+push() vs unshift()
+
+## Functions, callback, en objects
+
+Gaat voort op het huidige API voorbeeld (les-3)
+
+```
+const obj = {
+    f1: ()=>{}
+    f2: ()=>{}
+    f3: ()=>{}
+}
+```
+
+## Drag sorting?
+
+Goede oefeningen op array bewerkingen, met indexOf() en splice(), en ondragstart enz
+
+## Sorteer algoritme in browser
+
+zo bubble sort ofzo
+
+## Backend oefening
+
+Node js
+
+Misschien beter, omdat dit voortgaat op les 3?
+
+Expressjs API bouwen, en koppelen met frontend
+
+get('api/')
+en dan iets met post?
+

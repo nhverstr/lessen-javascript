fcf7a4118c6fd26cf068a5adf55206dda5b58efc
virgke <virginie.wylleman@telenet.be>
2024-04-22 17:01:43 +0200

    deel drie

--- les-4/script.js
+++ les-4/script.js
@@ -54,7 +54,6 @@ console.log(techString);
 
 
 // code deel 2
-
 let shortNatureArr = natureArr.filter(checkWordLength);
 
 function checkWordLength(word) {
@@ -63,23 +62,58 @@ function checkWordLength(word) {
   }
 }
 /*@Nathan: wat ik niet snap is hoe js nu weet wat ik bedoel met word? */
-console.log(shortNatureArr);
+console.log("shortNatureArr has all words of natureArr that have less then six characters", shortNatureArr);
 
-/**
- * Deel 3
+
+/* Deel 3
  * Output het laatste woord met 4 letters in techArr
+ */
+
+/*methode 1 */
+let techArrFour = techArr.filter(checkWordLength2);
+
+function checkWordLength2(word) {
+  if (word.length === 4) {
+    return word;
+  }
+};
+
+let lastWord = techArrFour.slice(-1).toString();
+console.log("lastWord has the last four character word of techArr:", lastWord);
+
+/*methode 2 */
+let lastWord2 = techArr.findLast(checkWordLength2);
+console.log("lastWord2 has the last four character word of techArr:", lastWord2);
+
+/*
  * Output techArr zonder dat laatste woord
- *
- * Output de reverse van deze array.
+ */
+let techArrSpliced = techArr;
+techArrSpliced.splice(techArr.indexOf(lastWord2), 1);
+console.log("techArrSpliced is techArr without lastWord", techArrSpliced);
+/* Output de reverse van deze array.*/
+let techArrReverse = techArr.reverse();
+console.log("techArrReverse is the reverse array of techArr", techArrReverse);
+/*
  *
  * Output het eerste woord met 4 letters in techArr
+ */
+let firstWord = techArrFour[0].toString();
+let firstWord2 = techArrFour.slice(0,1).toString();
+let firstWord3 = techArrFour.find(checkWordLength2) /*find(), finds the first instance */;
+console.log("The result of several way to get the first four letter word of techArr:", firstWord, firstWord2, firstWord3);
+ /* 
  * Output techArr zonder dat eerste woord
+ */
+
+let techArrSpliced2 = techArr;
+techArrSpliced2.splice(techArr.indexOf(firstWord), 1);
+console.log(techArrSpliced2);
+/*
  *
  * Tip: als je deze allemaal na elkaar uitvoert, kom je in de problemen. Gebruik een temporary array
  * om je originele bewaren
  */
-
-// code deel 3
 
 /**
  * Deel 4
